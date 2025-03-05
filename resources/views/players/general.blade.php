4a8ccaf491d0d84bc18c94be386a1be3626bc5b6
mikihouse1023 <167415128+mikihouse1023@users.noreply.github.com>
2025-03-05 20:20:00 +0900

    Add files via upload

---
+++ resources/views/players/general.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>選手リスト</title>
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+    <p class="top"><strong>選手リスト</strong></p>
+    <div class="logout_button_form">
+        <button class="logout_button" onclick="location.href='{{ route('login') }}'">ログアウト</button>
+    </div>
+
+<body>
+
+    <table class=detail>
+        <tr>
+            <th class=No>No</th>
+            <th class=uniform>背番号</th>
+            <th class=position>ポジション</th>
+            <th class=club>所属</th>
+            <th class=name>名前</th>
+            <th class=player_country>国</th>
+            <th class=birth>誕生日</th>
+            <th class=height>身長</th>
+            <th class=weight>体重</th>
+            <th class=詳細>
+
+                </td>
+        </tr>
+        <!--単数形($player)を使用し、個々のプレイヤーを指し示すための一時的な変数名にする($playersだとエラーが発生)-->
+        @foreach ($players as $player)
+        <tr>
+            <td>{{ $player->id }}</td>
+            <td>{{ $player->uniform_num }}</td>
+            <td>{{ $player->position }}</td>
+            <td>{{ $player->club }}</td>
+            <td>{{ $player->name }}</td>
+            <td>{{ $player->country_name }}</td>
+            <td>{{ $player->birth }}</td>
+            <td>{{ $player->height }}</td>
+            <td>{{ $player->weight }}</td>
+            <!--<td><a class="details"  href="{{ route('players.show', $player->id) }}">詳細</a></td>-->
+            <td><button class="details" id=detail_button onclick="location.href='{{ route('players.show', $player->id) }}'">詳細</button></td>
+
+        </tr>
+
+        @endforeach
+    </table>
+    <!--ページネーションはテーブルの下-->
+    <div class="pagination">
+
+        {{ $players->links() }}
+
+    </div>
+
+
+
+</body>
+
+</html>
